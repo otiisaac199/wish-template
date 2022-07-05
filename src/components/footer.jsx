ded0054c461b8d864b4243378b2398e75a824c5f
Isaac <otichinazaisaac19@gmail.com>
2022-07-05 11:38:55 +0100

    pending

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class Footer extends Component {
   state = {};
   render() {
-    return <React.Fragment></React.Fragment>;
+    return (
+      <React.Fragment>
+        <div class="footer-container"></div>
+      </React.Fragment>
+    );
   }
 }
 
